414b14baa80223834634c9dde7672839d0d09e65
hgkim0728 <hgkim0728@gmail.com>
2025-04-08 12:30:01 +0900

    Feat: 형태 변환 스크립터블 오브젝트 추가
    각 형태의 정보를 담는 용도의 스크립터블 오브젝트 추가

---
+++ Assets/03.Scripts/Player/PlayerFormData.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+[System.Serializable]
+public class FormData
+{
+    // 변신 형태 이름
+    [SerializeField] private string formName;
+    public string FormName {  get { return formName; } }
+    // 애니메이터 컨트롤러
+    // 애니메이션 추가하기 전까지는 비활성화
+    //[SerializeField] AnimatorController animCon;
+    //public AnimatorController AnimCon { get { return animCon; } }
+    // 점프력
+    [SerializeField] private float jumpForce;
+    public float JumpForce { get { return jumpForce; } }
+    // 크기
+    [SerializeField] private float size;
+    public float Size { get { return size; } }
+    // 힘
+    [SerializeField] private float force;
+    public float Force { get { return force; } }
+    // 무게
+    [SerializeField] private float weight;
+    public float Weight { get { return weight; } }
+    // 해금 상태
+    public bool IsActive = false;
+}
+
+[CreateAssetMenu(fileName = "new PlayerForm Data", menuName = "PlayerForm Data")]
+public class PlayerFormData : ScriptableObject
+{
+    [SerializeField] private List<FormData> playerFormList;
+    public List<FormData> PlayerFormList { get { return playerFormList; } }
+}
