4314a5fde2e451bb3afef24c9ab3a15e872d7a13
Kim Tae-gyeom <114921436+BeautifulMaple@users.noreply.github.com>
2025-04-10 18:02:07 +0900

    WIP: 벽 파괴

--- Assets/03.Scripts/Obstacles/BreakWall.cs
+++ Assets/03.Scripts/Obstacles/BreakWall.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class BreakWall : MonoBehaviour
 {
+    // 벽을 부술 수 있는 오브젝트
+    // 벽을 부술 수 있는 오브젝트는 Player의 형태변화 강아지
+
     public Collider2D wallCollider;
+
+    public Player player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.TryGetComponent(out PlayerFormController playerFormController))
+        {
+            if (playerFormController.CurFormData.FormName == "Dog")
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
+
