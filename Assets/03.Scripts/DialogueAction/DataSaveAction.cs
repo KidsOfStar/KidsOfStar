f3fdb7297b0fcc85348d91a5cbdaa425602b9d9e
Kim Tae-gyeom <114921436+BeautifulMaple@users.noreply.github.com>
2025-04-15 12:50:19 +0900

    WIP: 저장 및 로드 진행 중

--- Assets/03.Scripts/DialogueAction/DataSaveAction.cs
+++ Assets/03.Scripts/DialogueAction/DataSaveAction.cs
@@ -1,9 +1,19 @@
 using MainTable;
+using System;
 
 public class DataSaveAction : IDialogActionHandler
 {
     public void Execute(DialogData playerData)
     {
-        EditorLog.Log("데이터 세이브 미구현");
+        EditorLog.Log("데이터 세이브 시작");
+
+        // 슬롯이랑 [난이도] 챕터 날짜 시간 string
+        int sllotIndex = 0; // 외부에서 지정하기
+
+        Managers.Instance.SaveManager.Save(sllotIndex, (timeStamp) =>
+        {
+            // SaveData.cs 의 timeStamp 포맷은 "Chapter01. 25-03-22 14:14:38"
+            string slotName = timeStamp;
+        });
     }
 }
--- Assets/03.Scripts/UI/Popup/SaveLoadPopup.cs
+++ Assets/03.Scripts/UI/Popup/SaveLoadPopup.cs
@@ -1,18 +1,34 @@
+using TMPro;
 using UnityEngine.UI;
 
 public class SaveLoadPopup : PopupBase
 {
-    //public Button closeBtn;
+    public TextMeshProUGUI saveTimeText;
+    public Button saveButton;
+    public Button loadButton;
+    public Button deleteButton;
 
+    private int selectedIndex = 0; 
     // Start is called before the first frame update
     protected override void Start()
     {
         base.Start();
-        //closeBtn.onClick.AddListener(OnClickClosebtn);
+
+        
     }
 
-    //public void OnClickClosebtn()
-    //{
-    //    HideDirect();
-    //}
+    public void OnSaveButtonClick(int index)
+    {
+        Managers.Instance.SaveManager.Save(index, (timeStamp) =>
+        {
+            saveTimeText.text = timeStamp;
+
+        });
+    }
+
+    public void OnLoadButtonClick(int index)
+    {
+        string dummyTimeStamp = "";
+        Managers.Instance.SaveManager.Load(index, dummyTimeStamp);
+    }
 }
