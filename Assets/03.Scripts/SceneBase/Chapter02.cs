86cb9cbfbd2287608fddec840493daed1ef6dcf9
윤동영 <ehddud9608@naver.com>
2025-04-16 12:22:44 +0900

    Fix: 챕터재생시 컷씬 재생 오류 해결
    Fix: 챕터재생시 컷씬 재생 오류 해결

--- Assets/03.Scripts/SceneBase/Chapter02.cs
+++ Assets/03.Scripts/SceneBase/Chapter02.cs
@@ -6,6 +6,6 @@ public class Chapter02 : MonoBehaviour
 {
     private void Start()
     {
-        Managers.Instance.CutSceneManager.PlayCutScene("Chapter02");
+        Managers.Instance.CutSceneManager.PlayCutScene(CutSceneType.Chapter02.ToString());
     }
 }
