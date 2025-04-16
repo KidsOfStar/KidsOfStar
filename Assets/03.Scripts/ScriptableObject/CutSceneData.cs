a53ea7c98b5d23c083297e73237ee8611b4cd563
윤동영 <ehddud9608@naver.com>
2025-04-16 19:15:03 +0900

    Feat: 레터박스 기능 추가
    레터박스 기능 추가

--- Assets/03.Scripts/ScriptableObject/CutSceneData.cs
+++ Assets/03.Scripts/ScriptableObject/CutSceneData.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 [Serializable]
 public class CutSceneData
 {
-    [field: SerializeField] public int CutSceneID { get; private set; }
     [field: SerializeField] public int[] DialogIndexes { get; private set; }
     [field: SerializeField] public NPC[] Npcs { get; private set; }
 }
