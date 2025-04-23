f1ac04cedfcf79244fbc1179a3866b64130cf065
윤동영 <ehddud9608@naver.com>
2025-04-23 19:52:14 +0900

    Chore: 머지용 커밋
    머지용 커밋

--- Assets/03.Scripts/SceneBase/Chapter02Base.cs
+++ Assets/03.Scripts/SceneBase/Chapter02Base.cs
@@ -7,7 +7,5 @@ public class Chapter02Base : SceneBase
         Managers.Instance.SoundManager.PlayBgm(BgmSoundType.InForest);
 
         //Managers.Instance.CutSceneManager.PlayCutScene(CutSceneType.Rescued.GetName());
-        //Managers.Instance.UIManager.Show<TreePuzzlePopup>();
-        //Managers.Instance.UIManager.Hide<TreePuzzlePopup>();
     }
 }
