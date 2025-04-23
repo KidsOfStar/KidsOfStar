aef405c6840610676f5b63ea9f8d6988396baa2b
Kim Tae-gyeom <114921436+BeautifulMaple@users.noreply.github.com>
2025-04-23 17:15:14 +0900

    Rename : DashGame으로 변경

--- Assets/03.Scripts/UI/UITool/InteractableHandler.cs
+++ Assets/03.Scripts/UI/UITool/InteractableHandler.cs
@@ -2,26 +2,51 @@ using UnityEngine;
 
 public class InteractableHandler : MonoBehaviour
 {
+    private bool playerInRange = false;
     public SkillBTN skillBTN;
+    private DashGame dashGame;
 
     // Start is called before the first frame update
     void Start()
     {
         skillBTN = Managers.Instance.UIManager.Get<PlayerBtn>().skillPanel;
+        dashGame = FindObjectOfType<DashGame>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            skillBTN.ShowInteractionButton(true);
+            playerInRange = true;
+            skillBTN.ShowInteractionButton(true); // 버튼 표시
+            skillBTN.OnInteractBtnClick += OnPlayerInteract;
         }
     }
+
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            skillBTN.ShowInteractionButton(false);
+            playerInRange = false;
+            skillBTN.ShowInteractionButton(false); // 버튼 숨김
+            skillBTN.OnInteractBtnClick -= OnPlayerInteract;
         }
     }
+
+    private void OnPlayerInteract()
+    {
+        if (!playerInRange)
+        {
+            dashGame.StartGame(); // StartGame 호출
+            skillBTN.ShowInteractionButton(true);
+            skillBTN.OnInteractBtnClick -= OnPlayerInteract;
+        }
+        else
+        {
+            dashGame.EndGame(); // EndGame 호출
+            skillBTN.ShowInteractionButton(false);
+            skillBTN.OnInteractBtnClick -= OnPlayerInteract;
+        }
+    }
+
 }
