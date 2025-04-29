89b7bd52fd817675b42ed41273991ba918ebeccf
Kim Tae-gyeom <114921436+BeautifulMaple@users.noreply.github.com>
2025-04-29 14:27:56 +0900

    Remove : 202 씬 삭제

--- Assets/03.Scripts/SceneBase/Chapter02Base.cs
+++ Assets/03.Scripts/SceneBase/Chapter02Base.cs
@@ -4,13 +4,12 @@ public class Chapter02Base : SceneBase
 {
     protected override void InitSceneExtra(Action playIntroCallback)
     {
-        
         Managers.Instance.SoundManager.PlayBgm(BgmSoundType.InForest);
         playIntroCallback?.Invoke();
 
         SkillForm();
-
     }
+
     private void SkillForm()
     {
         var skillBtn = Managers.Instance.UIManager.Get<PlayerBtn>().skillPanel;
@@ -19,6 +18,5 @@ public class Chapter02Base : SceneBase
         Managers.Instance.GameManager.UnlockForm("Squirrel");
         skillUnlock.UnlockSkill("Squirrel");
         skillUnlock.ShowSkillBG(skillUnlock.squirrelBG); // 'Squirrel' 스킬 BG 활성화
-
     }
 }
