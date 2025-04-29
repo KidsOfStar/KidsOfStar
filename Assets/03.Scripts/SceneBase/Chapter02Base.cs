741e3d65d336de71560c3c96ab891876950eb4db
윤동영 <ehddud9608@naver.com>
2025-04-29 12:13:54 +0900

    Fix: 생명 주기 함수에 따른 초기화 변경
    생명 주기 오류에 따른 초기화 변경

--- Assets/03.Scripts/SceneBase/Chapter02Base.cs
+++ Assets/03.Scripts/SceneBase/Chapter02Base.cs
@@ -4,6 +4,7 @@ public class Chapter02Base : SceneBase
 {
     protected override void InitSceneExtra(Action playIntroCallback)
     {
+        
         Managers.Instance.SoundManager.PlayBgm(BgmSoundType.InForest);
         playIntroCallback?.Invoke();
 
