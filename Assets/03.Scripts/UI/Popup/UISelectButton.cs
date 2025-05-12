c627440ccf9372ddf201e4652e8fe34865d46715
Cindy0914 <98309090+Cindy0914@users.noreply.github.com>
2025-05-12 22:28:10 +0900

    Feat: 하이라이트 셀렉트 액션
    - 중요 스토리 분기점 선택지 액션 추가
    - 현재는 텍스트 애니메이션만 있음
    - 효과음 생기면 추가 할 예정

--- Assets/03.Scripts/UI/Popup/UISelectButton.cs
+++ Assets/03.Scripts/UI/Popup/UISelectButton.cs
@@ -7,27 +7,25 @@ using UnityEngine.UI;
 public class UISelectButton : MonoBehaviour
 {
     [SerializeField] private Button button;
-    [SerializeField] private TextMeshProUGUI selectionText;
     [SerializeField] private TypewriterByCharacter typewriter;
     
     public void DefaultInit(int index, Action<int> onClickAction, string text)
     {
-        typewriter.useTypeWriter = false;
-        selectionText.text = text;
         button.onClick.AddListener(() => onClickAction(index));
         gameObject.SetActive(true);
+        typewriter.useTypeWriter = false;
+        typewriter.ShowText(text);
     }
 
     public void HighlightInit(int index, Action<int> onClickAction, string text)
     {
-        typewriter.useTypeWriter = true;
-        selectionText.text = text;
+        typewriter.ShowText(text);
         button.enabled = false;
         button.onClick.AddListener(() => onClickAction(index));
         typewriter.onTextShowed.AddListener(() => button.enabled = true);
         
         gameObject.SetActive(true);
-        typewriter.ShowText(text);
+        typewriter.StartShowingText();
     }
     
     private void OnDisable()
