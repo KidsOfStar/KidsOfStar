1cd4f09e59bd8934f23e20627c46440e2327ab41
Kim Tae-gyeom <114921436+BeautifulMaple@users.noreply.github.com>
2025-05-12 16:59:09 +0900

    Fix : 챕터 인트로 글씨 조정

---
+++ Assets/03.Scripts/SceneBase/Chapter0502Base.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chapter0502Base : SceneBase
+{
+    protected override void CutSceneEndCallback()
+    {
+        PlayChapterIntro();
+    }
+
+    protected override void InitSceneExtra(Action callback)
+    {
+        SkillForm();
+    }
+
+    private void SkillForm()
+    {
+        Managers.Instance.GameManager.UnlockForm(PlayerFormType.Hide);
+    }
+
+
+}
