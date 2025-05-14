181ae9dcdaba200ffe02902808be45a6bde78a8c
Kim Tae-gyeom <114921436+BeautifulMaple@users.noreply.github.com>
2025-05-14 11:25:25 +0900

    Add : 스크립트 복구
    TilePatrolNPC.cs
    VentDown.cs

--- Assets/03.Scripts/Chapter_05/Tallman.cs
+++ Assets/03.Scripts/Chapter_05/Tallman.cs
@@ -11,15 +11,53 @@ public class Tallman : MonoBehaviour
     private Coroutine warningCoroutine;                     // 경고 코루틴
 
 
-    // Start is called before the first frame update
-    void Start()
+    private void OnInteraction()
     {
-        
+        if (warningCoroutine != null) return;
+
+        var player = Managers.Instance.GameManager.Player;
+        string currentForm = player?.FormControl?.CurFormData?.FormName?.ToLower();
+
+        if (string.IsNullOrEmpty(currentForm)) return;
+
+        // 동물 폼인 경우에만 경고 시작
+        //if (animalForms.Contains(currentForm))
+        //{
+        //    warningCoroutine = StartCoroutine(ShowWarning());
+        //    Debug.Log($"StartCoroutine(ShowWarning())");
+        //}
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator ShowWarning()
     {
-        
+        var player = Managers.Instance.GameManager.Player;
+
+        if (bubbleTextPrefab == null) yield break;
+
+        bubbleTextInstance = Instantiate(bubbleTextPrefab, player.transform);
+        bubbleTextInstance.transform.localPosition = new Vector3(0, 2f, 0);
+
+        var bubbleText = bubbleTextInstance.GetComponentInChildren<DoorPopup>();
+
+        if (bubbleText != null)
+        {
+            bubbleText.SetText("정체를 들킬 것 같다.");
+            Debug.Log("정체를 들킬 것 같다.");
+        }
+        yield return new WaitForSeconds(1f);
+
+        if (bubbleText != null)
+        {
+            bubbleText.SetText("당장 피해야 한다.");
+        }
+        yield return new WaitForSeconds(1f);
+
+        if (bubbleText != null)
+        {
+            bubbleText.SetText("위험하다.");
+        }
+        yield return new WaitForSeconds(1f);
+
+        //TriggerCaughtEnding();
     }
 }
