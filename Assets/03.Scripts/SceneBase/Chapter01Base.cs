132b04b71202913373491e5b4adc6e1f20da9a7a
윤동영 <ehddud9608@naver.com>
2025-05-16 11:22:46 +0900

    WIP: 머지용 커밋
    머지용 커밋

--- Assets/03.Scripts/SceneBase/Chapter01Base.cs
+++ Assets/03.Scripts/SceneBase/Chapter01Base.cs
@@ -18,6 +18,7 @@ public class Chapter01Base : SceneBase
     
     protected override void InitSceneExtra(Action callback)
     {
+        Managers.Instance.CutSceneManager.PlayCutScene(CutSceneType.TheCatReturns);
         //Managers.Instance.CutSceneManager.PlayCutScene(CutSceneType.FallingDown, callback);
         callback?.Invoke();
         sceneEventTrigger.Init();
