aa915cba69e2bddda812aa8d074da553717bc549
Kim Tae-gyeom <114921436+BeautifulMaple@users.noreply.github.com>
2025-05-19 16:47:35 +0900

    Test : 씬 노래 확인 중

--- Assets/03.Scripts/SceneBase/Chapter0502Base.cs
+++ Assets/03.Scripts/SceneBase/Chapter0502Base.cs
@@ -12,16 +12,6 @@ public class Chapter0502Base : SceneBase
 
     protected override void InitSceneExtra(Action callback)
     {
-        SkillForm();
-        
         Managers.Instance.GameManager.UpdateProgress(); // 진행도 2
-
     }
-
-    private void SkillForm()
-    {
-        Managers.Instance.GameManager.UnlockForm(PlayerFormType.Hide);
-    }
-
-
 }
