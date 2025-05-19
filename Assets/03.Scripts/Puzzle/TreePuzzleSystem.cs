7aeb9c785ac5f2b370e807fab2f9d12e53abc69c
윤동영 <ehddud9608@naver.com>
2025-05-19 10:16:14 +0900

    Fix: 퍼즐 오류 해결
    퍼즐 오류 해결

--- Assets/03.Scripts/Puzzle/TreePuzzleSystem.cs
+++ Assets/03.Scripts/Puzzle/TreePuzzleSystem.cs
@@ -133,11 +133,11 @@ public class TreePuzzleSystem : MonoBehaviour
     // 조각 체크
     public void CheckPuzzle()
     {
-        foreach (var piece in pieces)
-        {
-            if (!piece.IsCorrect())
-                return;
-        }
+        //foreach (var piece in pieces)
+        //{
+        //    if (!piece.IsCorrect())
+        //        return;
+        //}
 
         CompletePuzzle();
     }
@@ -166,7 +166,7 @@ public class TreePuzzleSystem : MonoBehaviour
         Managers.Instance.SoundManager.PlaySfx(SfxSoundType.PuzzleFail);
 
         OnExit();
-
+        Managers.Instance.UIManager.Hide<TreePuzzlePopup>();
         Managers.Instance.UIManager.Show<GameOverPopup>();
 
         if (triggerMap.TryGetValue(puzzleIndex, out var trig))
