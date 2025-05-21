02c58b3622ec35b15ec9c418b074011e117d24af
Cindy0914 <98309090+Cindy0914@users.noreply.github.com>
2025-05-21 17:49:52 +0900

    Feat: ConstantWidthCamera 수정

--- Assets/03.Scripts/Entity/ConstantWidthCamera.cs
+++ Assets/03.Scripts/Entity/ConstantWidthCamera.cs
@@ -13,34 +13,31 @@ public class ConstantWidthCamera : MonoBehaviour
     private void Awake()
     {
         cam = GetComponent<Camera>();
-        if (!cam.orthographic)
 
-        // 최초 설정
-        UpdateOrthographicSize();
+        if (!cam.orthographic)
+            UpdateOrthographicSize();
         lastScreenWidth = Screen.width;
         lastScreenHeight = Screen.height;
     }
 
-#if TEST_BUILD
     private void Update()
     {
         if (lastScreenWidth != Screen.width || lastScreenHeight != Screen.height)
         {
             // 화면 크기가 변경되었을 때 orthographicSize를 업데이트
             UpdateOrthographicSize();
-        }
+            lastScreenWidth = Screen.width;
+            lastScreenHeight = Screen.height;
 
-        UpdateOrthographicSize();
-        lastScreenWidth = Screen.width;
-        lastScreenHeight = Screen.height;
+            EditorLog.Log("UpdateOrthographicSize");
+        }
     }
-#endif
 
     private void UpdateOrthographicSize()
     {
         // 화면 비율(width/height)
         float screenAspect = (float)Screen.width / Screen.height;
-        
+
         // orthographicSize는 "세로 절반(world unit)"이므로,
         // targetWorldWidth / screenAspect = world 단위 세로 전체 길이
         // 이를 절반으로 잘라서 orthographicSize로 세팅
