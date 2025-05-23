577d206b76d4323a643445b4c6274dba776379a1
Cindy0914 <98309090+Cindy0914@users.noreply.github.com>
2025-05-23 11:01:49 +0900

    fix

--- Assets/03.Scripts/Entity/BgmLayeredFader.cs
+++ Assets/03.Scripts/Entity/BgmLayeredFader.cs
@@ -4,16 +4,13 @@ using UnityEngine;
 
 public class BgmLayeredFader : MonoBehaviour
 {
-    // TODO: 503 SceneBase에서 풀매니저로 먼저 생성시킬 것. Instantiate 괘오래걸림
-    // TODO: GameManager에 넣을 수 밖에 없지 않냐..
-    
     [Header("Audio")]
     [SerializeField] private AudioSource[] mainSources;
     [SerializeField] private AudioSource[] loopEffects;
 
     [Header("Loop Time")]
     [SerializeField] private float mainLoopTime = 192.5f;
-    [SerializeField] private float bufferOffset = 0.1f;
+    [SerializeField] private float bufferOffset = 0.5f;
     
     [Header("Dialog")]
     [SerializeField] private int strMelodyIndex = 5012;
